ca58d88874845b9912f24664330d33bb2be86bb8
Anuj5588 <ak4826636@gmail.com>
2026-01-31 14:01:27 +0530

    added the hosted url of server

--- app/services/api.js
+++ app/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000"
+  baseURL: "https://job-importer-be.vercel.app"
 });
 
 export const runImport = () => api.post("/api/imports/run");
